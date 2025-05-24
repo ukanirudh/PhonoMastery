c12466bc7263633606c248f46941ee413d15f78e
ukanirudh <anirudh.anirudh93@gmail.com>
2025-05-24 18:34:18 +0530

    Anirudh: adding capacitor for cross platform support

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.phonomastery',
+  appName: 'phonomastery',
+  webDir: 'dist'
+};
+
+export default config;
